56c5466a200316764cbb38098efcfce3469d5172
Victor DUMENIL <victor.dumenil01@gmail.com>
2026-03-30 18:19:35 -0400

    sh

--- MC4BRC_90Y.sh
+++ MC4BRC_90Y.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --nodelist=ubuntu22
 #SBATCH --partition=htc_cpu
-#SBATCH --cpus-per-task=6
-#SBATCH --job-name='MC4BRC 6He'
+#SBATCH --cpus-per-task=10
+#SBATCH --job-name='MC4BRC 90Y'
 #SBATCH --output=%x.%J.out
 #SBATCH --error=%x.%J.out
 #SBATCH --mail-user=dumenil@lpccaen.in2p3.fr
